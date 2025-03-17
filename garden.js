94cffa022f7f2d6dd4a8b85ffd038d518e73c1cd
ameenstudio <200854608+ameenstudio@users.noreply.github.com>
2025-03-16 19:58:26 -0600

    info update

--- garden.js
+++ garden.js
@@ -4,5 +4,6 @@ const area = (PI * radius * radius)
 let eachPlantarea = 0.8;
 let MaxPlants =;
 let totalarea= (area/eachPlantarea)
+//The garden starts with 20 plants, and the number doubles every week.
 
     console.log(`total area` + area)
