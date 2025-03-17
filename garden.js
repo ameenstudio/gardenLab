3ffc4f20787caa68a6d169032d813bb705bc3778
ameenstudio <200854608+ameenstudio@users.noreply.github.com>
2025-03-16 19:53:44 -0600

    fist commit

---
+++ garden.js
@@ -0,0 +1,8 @@
+const PI = 3.14415;
+const radius = 5
+const area = (PI * radius * radius)
+let eachPlantarea = 0.8;
+let MaxPlants =;
+let totalarea= (area/eachPlantarea)
+
+    console.log(`total area` + area)
